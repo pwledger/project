5538a8b9f3ae83766565c38bf23148a237a9d023
pwledger <108314068+pwledger@users.noreply.github.com>
2022-07-14 17:43:57 +0900

    이미지출력전까지

--- 노드파일/main.js
+++ 노드파일/main.js
@@ -1,10 +1,10 @@
-var http = require("http");  //asoji
+var http = require("http");  
 var fs = require('fs');
 var url =require('url')
 
 var app = http.createServer(function(request , response){
     var _url = request.url;
-    var queryData = url.parse(_url, true).query;
+    var queryData = url.parse(_url, true).query; // ?id= name
     if (_url == '/'){
         _url = '/index.html'
     }
@@ -15,6 +15,29 @@ var app = http.createServer(function(request , response){
     }
     response.writeHead(200);
     //response.end(fs.readFileSync(__dirname + _url));
-    response.end(queryData.id)  
+    var title = queryData.id
+    fs.readFile(`./data/${title}`, 'utf8' , (err, data) => {
+        templeate = 
+        `
+        <html>
+        <head>
+            <meta charset="utf-8">
+    
+        </head>
+        <body>
+            <h1> ${title} </h1>
+            <p>
+                <ul>
+                    <li><a href="/?id=html"> html </a></li>
+                    <li><a href="/?id=css"> css </a></li>
+                    <li><a href="/?id=JavaScript"> JavaScript </a></li>
+                </ul>
+                ${data}
+            </p>
+            </body>
+        </html>
+        ` 
+        response.end(templeate)
+      })
 });
 app.listen(3000);
